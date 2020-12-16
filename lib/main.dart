1aa7efc4e8b8e7457d76ce3bbc040581cfab010d
Aaris-Kazi <aariskazi@gmail.com>
2020-12-16 18:13:59 +0530

    Small bug changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:splashscreen/splashscreen.dart';
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: new ThemeData.dark(),
     home: new MyApp(),
   ));
 }
@@ -17,10 +18,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
-      
       seconds: 10,
       // navigateAfterSeconds: new AfterSplash(),
-      
+
       title: new Text(
         'WhatsApp',
         style: new TextStyle(
@@ -29,9 +29,10 @@ class _MyAppState extends State<MyApp> {
           color: Colors.green[200],
         ),
       ),
-      
+
       image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
-      backgroundColor: Colors.white,
+
+      backgroundColor: Colors.black87,
       styleTextUnderTheLoader: new TextStyle(),
       photoSize: 100.0,
       onClick: () => print("Flutter Egypt"),
